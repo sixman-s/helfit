841dc1a0c2bcb46e5a394d61dc09388cfae7a228
jw_0819 <jw_0819@naver.com>
2023-03-06 16:41:31 +0900

    :sparkles: init

---
+++ back/helfit/src/main/java/sixman/helfit/HelfitApplication.java
@@ -0,0 +1,13 @@
+package sixman.helfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelfitApplication.class, args);
+	}
+
+}
