d4bee5cfa69f91b13e7814fffed5546e758e7dd4
gimtaehyeong <adsds126@gmail.com>
2023-03-06 23:45:48 +0900

    :sparkles: Calculator 클래스 구현

--- back/helfit/src/main/java/sixman/helfit/domain/user/entity/User.java
+++ back/helfit/src/main/java/sixman/helfit/domain/user/entity/User.java
@@ -1,11 +1,32 @@
 package sixman.helfit.domain.user.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "USERS")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
+    private String name;
+    private int age;
+    private double weight;
+    private double height;
+    private String sex;
+    private String goal;
+    private double bmr; //기초대사량임.
+    private double hb;  //기초대사량 x ActiveLevel 한값. 유지칼로리.
+    public User(int age, double weight, double height, String sex) {
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+        this.sex = sex;
+    }
+
+
 }
