8c055e8f0d35a7edc5cb779c6f4a3ff87d8813c5
MODAC0 <modac0302@gmail.com>
2023-03-10 00:45:17 +0900

    🔨 fe/LNB css 완성 및 기능 추가 (커뮤니티 폴더 링크는 추후 작업)

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,7 +1,11 @@
+import { LNB } from '@/components/LNB';
 import { Inter } from 'next/font/google';
-
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <div>테스트 텍스트입니다</div>;
+  return (
+    <div>
+      <LNB />
+    </div>
+  );
 }
---
+++ front/src/pages/map.tsx
@@ -0,0 +1,3 @@
+const Map = () => {};
+
+export default Map;
