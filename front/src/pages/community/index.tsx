252e2007e45759b10bb35f64fa627a4736692eee
HooniKim <tpgns2289@naver.com>
2023-03-12 19:31:20 +0900

    🎉 fe/Community_layout branch시작

--- front/src/pages/community/index.tsx
+++ front/src/pages/community/index.tsx
@@ -1,13 +1,23 @@
+import style from '../../styles/Community/P_Community.module.css';
 import Layout from '@/components/MainLayout';
+import Categoty from '@/components/Community/Category';
+import Searchbar from '@/components/Community/SearchBar';
+import Popular from '@/components/Community/Popular';
 
 const Community = () => {
-  const style = {
-    height: 1000,
-    background: '#ddd'
-  };
   return (
     <Layout>
-      <h1 style={style}>contents</h1>
+      <div className={style.Container}>
+        <div className={style.Searchbar}>
+          <Searchbar />
+        </div>
+        <div className={style.Category}>
+          <Categoty />
+        </div>
+        <div className={style.Popular}>
+          <Popular />
+        </div>
+      </div>
     </Layout>
   );
 };
