fc7d4e48178d45f7ce7bb8bd580f68c69e652f04
HooniKim <tpgns2289@naver.com>
2023-03-12 19:32:14 +0900

    ⚡sementic-ui import

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import '../styles/global-style.css';
+import 'semantic-ui-css/semantic.min.css';
 import '../styles/variables.css';
 
 export default function App({ Component, pageProps }: AppProps) {
