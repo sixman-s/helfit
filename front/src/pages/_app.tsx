1130c1c85cfc09f577da8afefbbde2109d098217
HooniKim <tpgns2289@naver.com>
2023-03-13 15:36:21 +0900

    📝 editor 전역으로 css 스타일링

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -3,6 +3,7 @@ import '../styles/global-style.css';
 import 'semantic-ui-css/semantic.min.css';
 import '../styles/variables.css';
 import '../styles/calender/C_calenderApi.css';
+import '../styles/Editor.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
