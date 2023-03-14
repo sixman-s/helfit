fa5164b38bd647d9e7562677b100ca2e498697e9
HooniKim <tpgns2289@naver.com>
2023-03-14 17:38:27 +0900

    🎉 signup 브랜치 시작

--- front/src/components/Community/signup/SignupBox.tsx
+++ front/src/components/Community/signup/SignupBox.tsx
@@ -1,44 +1,100 @@
-import style from '../../styles/Signup/C_SignupBox.module.css';
-import OAuthBox from '../../loginc/OAuth';
-import Btn from '../../loginc/Buttons';
+import axios from 'axios';
+import style from '../../../styles/Signup/C_SignupBox.module.css';
+import OAuthBox from '@/components/loginc/OAuth';
+import Btn from '@/components/loginc/Buttons';
 import { useForm } from 'react-hook-form';
+import { useRouter } from 'next/router';
 
 type SignupForm = {
   userID: string;
   email: string;
   password: string;
   name: string;
-  birthdate: string;
+  birth: string;
   nickname: string;
 };
 
 const SignupBox = () => {
-  const { register, handleSubmit } = useForm<SignupForm>();
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<SignupForm>();
 
-  const onSubmit = (data: SignupForm) => console.log(data);
+  const onSubmit = (data: SignupForm) => {
+    axios
+      .post(
+        'http://ec2-3-34-96-242.ap-northeast-2.compute.amazonaws.com/api/v1/users/signup',
+        {
+          id: data.userID,
+          password: data.password,
+          email: data.email,
+          nickname: data.nickname,
+          birth: data.birth
+        }
+      )
+      .then((response) => {
+        router.push('/login');
+        console.log(response);
+      })
+      .catch((error) => console.log(error));
+  };
 
   return (
     <>
       <div className={style.container}>
         <div className={style.leftbox}>
+          {/* 아이디 입력칸 */}
+
           <div>
             <h5>아이디</h5>
             <input
               type='text'
               placeholder='아이디'
-              {...register('userID', { required: true })}
+              {...register('userID', {
+                required: true,
+                pattern: {
+                  value: /^[a-z0-9]{6,20}$/,
+                  message: '6~20글자 사이의 소문자 + 숫자의 조합이여야 합니다.'
+                }
+              })}
               className={style.signup__form}
             />
+            {errors.userID && (
+              <div style={{ color: 'red' }}>{errors.userID.message}</div>
+            )}
           </div>
+
+          {/* 비밀번호 입력칸 */}
+
           <div>
             <h5>비밀번호</h5>
             <input
               type='password'
               placeholder='비밀번호'
-              {...register('password', { required: true })}
+              {...register('password', {
+                required: true,
+                minLength: {
+                  value: 8,
+                  message: '8글자 이상이어야 합니다.'
+                },
+                pattern: {
+                  value:
+                    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+                  message:
+                    '대문자, 소문자, 숫자, 특수문자가 1개 이상씩 포함되어야 합니다.'
+                }
+              })}
               className={style.signup__form}
             />
+            {errors.password && (
+              <div style={{ color: 'red' }}>{errors.password.message}</div>
+            )}
           </div>
+
+          {/* 이메일 입력칸 */}
+
           <div>
             <h5>이메일</h5>
             <input
@@ -49,6 +105,9 @@ const SignupBox = () => {
             />
           </div>
         </div>
+
+        {/* 생년월일 입력칸 */}
+
         <div className={style.rightbox}>
           <img className={style.logo} src={'assets/LoginP/logo.svg'} />
           <div>
@@ -56,11 +115,24 @@ const SignupBox = () => {
             <input
               type='text'
               placeholder='생년월일'
-              {...register('birthdate', { required: true })}
+              {...register('birth', {
+                required: true,
+                pattern: {
+                  value:
+                    /^(19[0-9][0-9]|20\d{2})(0[0-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0-1])$/,
+                  message: '올바른 생년월일 8글자를 입력해주세요'
+                }
+              })}
               className={style.signup__form}
             />
-            <h6 className={style.h6}>ex) 2000.01.01</h6>
+            <h6 className={style.h6}>ex) 20000101</h6>
+            {errors.birth && (
+              <div style={{ color: 'red' }}>{errors.birth.message}</div>
+            )}
           </div>
+
+          {/* 닉네임 입력칸 */}
+
           <div>
             <h5>닉네임</h5>
             <input
--- front/src/components/loginc/OAuth.tsx
+++ front/src/components/loginc/OAuth.tsx
@@ -1,17 +1,57 @@
 import style from '../../styles/Login/C_LoginBox.module.css';
+import axios from 'axios';
 
 const OAuthBox = () => {
+  const handleGoogleLogin = () => {
+    axios
+      .get(
+        'http://ec2-3-34-96-242.ap-northeast-2.compute.amazonaws.com/oauth2/authorization/google'
+      )
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleNaverLogin = () => {
+    axios
+      .get(
+        'http://ec2-3-34-96-242.ap-northeast-2.compute.amazonaws.com/oauth2/authorization/naver'
+      )
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleKakaoLogin = () => {
+    axios
+      .get(
+        'http://ec2-3-34-96-242.ap-northeast-2.compute.amazonaws.com/oauth2/authorization/kakao'
+      )
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <>
       <h3 className={style.OAuthText}>Login with</h3>
       <div className={style.OAuth2}>
-        <button className={style.OAuthBtn}>
+        <button className={style.OAuthBtn} onClick={handleGoogleLogin}>
           <img src='assets/LoginP/GoogleOAuth.png' height='55px' />
         </button>
-        <button className={style.OAuthBtn}>
+        <button className={style.OAuthBtn} onClick={handleNaverLogin}>
           <img src='assets/LoginP/NaverOAuth.png' height='55px' />
         </button>
-        <button className={style.OAuthBtn}>
+        <button className={style.OAuthBtn} onClick={handleKakaoLogin}>
           <img src='assets/LoginP/KakaoOAuth.png' height='55px' />
         </button>
       </div>
