c350f138e805bbbd0c43a439e1b6413ef28e0e33
MODAC0 <modac0302@gmail.com>
2023-03-16 14:21:36 +0900

    💄 비회원시 메인/캘린더 플로우 추가 / style 수정

--- front/src/components/main/NonMembers.tsx
+++ front/src/components/main/NonMembers.tsx
@@ -1,21 +1,20 @@
 import Link from 'next/link';
 import layout from '../../styles/main/C_infoLayout.module.css';
-import styled from '../../styles/main/C_infoLayout.module.css';
+import styled from '../../styles/main/C_nonMembers.module.css';
 
 const NonMembers = () => {
   return (
-    <form>
-      <div>
-        <p>로그인이 필요한 서비스입니다.</p>
+    <form className={layout.container}>
+      <div className={styled.container}>
+        <img className={styled.errIcon} src='/assets/mainP/error_icon.svg' />
+        <p className={styled.title}>로그인이 필요한 서비스입니다.</p>
         <Link href='/login'>
-          <button>login</button>
+          <button className={styled.loginBtn}>Login</button>
         </Link>
-      </div>
-      <div>
-        <p>
+        <p className={styled.signupGuide}>
           아직 회원이 아니시면
           <Link className={layout.moreBtn} href='/signup'>
-            {' 여기'}
+            여기
           </Link>
           를 클릭해주세요.
         </p>
