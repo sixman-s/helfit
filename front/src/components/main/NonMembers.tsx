b3908efa99a66c6c0e9a48381da6d162836ea305
MODAC0 <modac0302@gmail.com>
2023-03-16 10:43:15 +0900

    💄 비회원인 페이지 css 추가

--- front/src/components/main/NonMembers.tsx
+++ front/src/components/main/NonMembers.tsx
@@ -4,15 +4,15 @@ import styled from '../../styles/main/C_infoLayout.module.css';
 
 const NonMembers = () => {
   return (
-    <form>
-      <div>
-        <p>로그인이 필요한 서비스입니다.</p>
+    <form className={styled.container}>
+      <div className={styled.guide}>
+        <p className={styled.guideText}>로그인이 필요한 서비스입니다.</p>
         <Link href='/login'>
-          <button>login</button>
+          <button className={styled.btnText}>login</button>
         </Link>
       </div>
-      <div>
-        <p>
+      <div className={styled.guide}>
+        <p className={styled.guide}>
           아직 회원이 아니시면
           <Link className={layout.moreBtn} href='/signup'>
             {' 여기'}
