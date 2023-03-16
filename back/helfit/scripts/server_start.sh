c806d8483f7e24cd58cd652d211f5744ea93be4d
jw_0819 <jw_0819@naver.com>
2023-03-16 18:17:26 +0900

    :bug: 배포 관련 파일 수정

--- back/helfit/scripts/server_start.sh
+++ back/helfit/scripts/server_start.sh
@@ -7,4 +7,4 @@ LOG_PATH="logs"
 LOG_FILE="application.log"
 LOGGED=$LOG_PATH/$LOG_FILE
 
-sudo nohup java -jar helfit-0.0.1-SNAPSHOT.jar >> $LOGGED 2>&1 &
+sudo nohup java -jar build/libs/helfit-0.0.1-SNAPSHOT.jar >> $LOGGED 2>&1 &
