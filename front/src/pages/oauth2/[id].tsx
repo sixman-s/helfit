542458e55f3d4a9a40118372217a198f2cd2fe77
HooniKim <tpgns2289@naver.com>
2023-03-16 11:40:23 +0900

    test

--- front/src/pages/oauth2/[id].tsx
+++ front/src/pages/oauth2/[id].tsx
@@ -1,3 +1,5 @@
+// 주석 추가
+
 // url: http://localhost:3000/oauth2/receive?access_token=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMDAyNDY4NTgwMjYxOTI2NTM1OTkiLCJyb2xlIjoiUk9MRV9VU0VSIiwiZXhwIjoxNjc5NjcyMzYyfQ.9x6MEY0Z7DO2TvwBv8SRl0wuLkVWDnegyiKENdlFqiE
 // access_token만 짤라서 로컬스토리지에 저장하고
 // 홈으로 이동시킨다.
