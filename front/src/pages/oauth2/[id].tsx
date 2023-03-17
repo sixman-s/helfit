cdf89fb3789579a01d933a930727c9b1a33bf6ef
HooniKim <tpgns2289@naver.com>
2023-03-17 10:03:50 +0900

    🔨 fe/dev router 수정

--- front/src/pages/oauth2/[id].tsx
+++ front/src/pages/oauth2/[id].tsx
@@ -1,8 +1,13 @@
 // url: http://localhost:3000/oauth2/receive?access_token=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMDAyNDY4NTgwMjYxOTI2NTM1OTkiLCJyb2xlIjoiUk9MRV9VU0VSIiwiZXhwIjoxNjc5NjcyMzYyfQ.9x6MEY0Z7DO2TvwBv8SRl0wuLkVWDnegyiKENdlFqiE
 // access_token만 짤라서 로컬스토리지에 저장하고
 // 홈으로 이동시킨다.
+
+import { useRouter } from 'next/router';
 import axios from 'axios';
+
 const OAuth2 = () => {
+  const router = useRouter();
+
   if (typeof window !== 'undefined') {
     // Get the URL from the address bar
     const url: string = window.location.href;
@@ -32,7 +37,7 @@ const OAuth2 = () => {
           console.log(error);
         });
 
-      window.location.replace('/');
+      router.push('/');
     }
   }
 };
