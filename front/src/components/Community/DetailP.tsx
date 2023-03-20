e07f5b5485a7bac0a49b0fb6db40d5374cad9e8a
HooniKim <tpgns2289@naver.com>
2023-03-20 22:43:46 +0900

    🔨 fe/Community 전체 게시글, 상세페이지에 태그 추가

--- front/src/components/Community/DetailP.tsx
+++ front/src/components/Community/DetailP.tsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from 'react';
 import UserNav from './C_Community/UserNav';
 import axios from 'axios';
 import { useRouter } from 'next/router';
+import Btn from '../loginc/Buttons';
+import Modal from './C_Community/DeletePost';
 
 interface BoardData {
   boardId: number;
@@ -19,12 +21,17 @@ interface BoardData {
 
 const DetailP = () => {
   const URL = process.env.NEXT_PUBLIC_URL;
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [writeCommnet, setWriteCommnet] = useState('');
   const [fetchedData, setFetchedData] = useState<BoardData | null>(null);
   const handleSubmit = (e: React.KeyboardEvent) => {
     // 댓글 작성 후 서버에 전송하는 로직 작성
     console.log('Comment submitted:', writeCommnet);
     setWriteCommnet('');
+  };
+
+  const handleDeletePostClick = () => {
+    setIsModalOpen(true);
   };
 
   const handleKeyDown = (e: React.KeyboardEvent) => {
@@ -37,7 +44,14 @@ const DetailP = () => {
     handleSubmit(keyboardEvent);
     console.log('Click!');
   };
-
+  const createdAtString = new Date(fetchedData?.createdAt)
+    .toLocaleDateString('en-KR', {
+      year: '2-digit',
+      month: '2-digit',
+      day: '2-digit'
+    })
+    .split('/')
+    .join('.');
   const router = useRouter();
   const { id } = router.query;
   const boardID = id ? parseInt(id[id.length - 1]) : null;
@@ -59,8 +73,9 @@ const DetailP = () => {
   }
 
   useEffect(() => {
+    const userID: number = JSON.parse(localStorage.UserInfo).userId;
     axios
-      .get(`${URL}/api/v1/board/${pageNumber}/2/${boardID}`)
+      .get(`${URL}/api/v1/board/${pageNumber}/${userID}/${boardID}`)
       .then((res) => setFetchedData(res.data))
       .catch((err) => console.log(err));
   }, [boardID]);
@@ -73,20 +88,45 @@ const DetailP = () => {
         </div>
         <div className={style.Category}>헬스 갤러리</div>
         <div className={style.Title}>{fetchedData?.title}</div>
-        <div className={style.PostNav}>
-          <div>작성자</div>
-          <div>{fetchedData?.createdAt}</div>
-          <div>조회수</div>
-          <div className={style.PostLike}>
-            <img
-              src='../../assets/Community/Like.svg'
-              className={style.PostLikeSVG}
+        <div className={style.Nav}>
+          <div className={style.PostNav}>
+            <div>작성자</div>
+            <div>{createdAtString}</div>
+            <div>조회수</div>
+            <div className={style.PostLike}>
+              <img
+                src='../../assets/Community/Like.svg'
+                className={style.PostLikeSVG}
+              />
+              <div>좋아요</div>
+            </div>
+          </div>
+          <div className={style.Buttons}>
+            <Btn
+              text='게시글 삭제'
+              type='submit'
+              className={style.ButtonD}
+              onClick={handleDeletePostClick}
             />
-            <div>좋아요</div>
+            {isModalOpen && (
+              <Modal
+                isOpen={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+              />
+            )}
+
+            <Btn text='게시글 수정' type='submit' className={style.ButtonU} />
           </div>
         </div>
         <div className={style.Content}>
-          <div className={style.Content_Text}>{fetchedData?.text}</div>
+          <div>
+            <div className={style.tag}>
+              {fetchedData?.tags.map((tag) => (
+                <span className={style.tagItem}>{tag.tagName}</span>
+              ))}
+            </div>
+            <div className={style.Content_Text}>{fetchedData?.text}</div>
+          </div>
           {fetchedData?.boardImageUrl && (
             <div style={{ width: '250px', height: '250px' }}>
               <img
