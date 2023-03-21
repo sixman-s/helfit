9f08c573adbdedd6fe9ea75b4673a556fe672461
gimtaehyeong <adsds126@gmail.com>
2023-03-21 16:20:18 +0900

    :sparkles: statcontroller test, calculatorControllerTest 1차

--- back/helfit/src/test/java/sixman/helfit/domain/calculator/controller/CalculatorControllerTest.java
+++ back/helfit/src/test/java/sixman/helfit/domain/calculator/controller/CalculatorControllerTest.java
@@ -1,4 +1,81 @@
 package sixman.helfit.domain.calculator.controller;
 
-public class CalculatorControllerTest {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import sixman.helfit.domain.calculator.dto.CalculatorDto;
+import sixman.helfit.domain.calculator.entity.Calculator;
+import sixman.helfit.domain.calculator.enums.ActivityLevel;
+import sixman.helfit.domain.calculator.enums.Goal;
+import sixman.helfit.domain.calculator.service.CalculatorService;
+import sixman.helfit.domain.physical.entity.Physical;
+import sixman.helfit.domain.physical.service.PhysicalService;
+import sixman.helfit.domain.user.entity.User;
+import sixman.helfit.restdocs.ControllerTest;
+import sixman.helfit.restdocs.annotations.WithMockUserCustom;
+import sixman.helfit.security.entity.UserPrincipal;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class CalculatorControllerTest extends ControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CalculatorService calculatorService;
+
+    @MockBean
+    private PhysicalService physicalService;
+
+    @WithMockUserCustom
+    @Test
+    @DisplayName("[테스트] 계산기 결과 등록")
+    void postResultTest() throws Exception {
+        //given
+        CalculatorDto.Post requestBody = new CalculatorDto.Post();
+        requestBody.setGoal(Goal.DIET);
+        requestBody.setActivityLevel(ActivityLevel.EXTRA_ACTIVE);
+
+        User user = new User();
+        user.setUserId(1L);
+        UserPrincipal userPrincipal = UserPrincipal.create(user);
+
+        Physical physical = new Physical();
+        physical.setWeight(78);
+        physical.setHeight(173);
+        physical.setBirth(1996-12-13);
+        physical.setGender(Physical.Gender.MALE);
+
+        Calculator calculator = new Calculator();
+        calculator.setCalculatorId(1L);
+        calculator.setResult(2600.0);
+
+
+
+        given(calculatorService.createResult(any(Calculator.class), any(User.class), any(Physical.class)))
+                .willReturn(calculator);
+        given(physicalService.findPhysicalByUserId(anyLong())).willReturn(physical);
+
+        //when/then
+        mockMvc.perform(post("/api/v1/calculate/{user-id}", user.getUserId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(requestBody)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.data.calculatorId").value(calculator.getCalculatorId()))
+                .andExpect(jsonPath("$.data.result").value(calculator.getResult()));
+    }
+
+
 }
--- back/helfit/src/test/java/sixman/helfit/domain/statistics/controller/StatControllerTest.java
+++ back/helfit/src/test/java/sixman/helfit/domain/statistics/controller/StatControllerTest.java
@@ -1,52 +1,64 @@
 package sixman.helfit.domain.statistics.controller;
 
-import com.google.gson.Gson;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.transaction.annotation.Transactional;
 import sixman.helfit.domain.statistics.Dto.StatDto;
+import sixman.helfit.domain.statistics.entity.Stat;
+import sixman.helfit.domain.statistics.mapper.StatMapper;
+import sixman.helfit.domain.statistics.service.StatService;
+import sixman.helfit.domain.user.entity.User;
+import sixman.helfit.restdocs.ControllerTest;
+import sixman.helfit.restdocs.annotations.WithMockUserCustom;
+import sixman.helfit.security.entity.ProviderType;
+import sixman.helfit.security.entity.RoleType;
+import sixman.helfit.security.entity.UserPrincipal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@Transactional
-@SpringBootTest
-@AutoConfigureMockMvc
-public class StatControllerTest {
+@WebMvcTest(StatControllerTest.class)
+public class StatControllerTest extends ControllerTest {
+    @MockBean
+    StatService statService;
+
+    @MockBean
+    StatMapper statMapper;
+
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private Gson gson;
+@Test
+@DisplayName("[테스트] 칼로리, 생성일자 : LOCAL")
+@WithMockUserCustom
+void getKcalStatTest() throws Exception {
+// given
+    List<StatDto.calendarResponse> responseList = new ArrayList<>();
+    StatDto.calendarResponse GetKcal = new StatDto.calendarResponse(1000, "2023-03-21");
+        responseList.add(GetKcal);
 
-    @Test
-    void getKcalStatTest() throws Exception{
-        //given
-        StatDto.calendarResponse calendarResponse = new StatDto.calendarResponse(1000, "2023-03-21");
-        String getContent = gson.toJson(calendarResponse);
+    given(statService.getCalendarByUserId(anyLong())).willReturn(new ArrayList<>());
+    given(statMapper.statListToStatDtoResponseList(anyList())).willReturn(responseList);
 
-        ResultActions getActions =
-                mockMvc.perform(
-                        get("api/v1/stat/calendar/{user-id}",2)
-                                .accept(MediaType.APPLICATION_JSON)
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(getContent)
-                );
-        String location = getActions.andReturn().getResponse().getHeader("Location");
-
-        //when/then
+    // when/then
         mockMvc.perform(
-                get(location)
+    get("/api/v1/stat/calendar/{user-id}", 1L)
                         .accept(MediaType.APPLICATION_JSON)
         )
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.data.kcal").value(calendarResponse.getKcal()))
-                .andExpect(jsonPath("$.data.recodedAt").value(calendarResponse.getRecodedAt()));
-    }
+            .andExpect(jsonPath("$.data[0].kcal").value(GetKcal.getKcal()))
+            .andExpect(jsonPath("$.data[0].recodedAt").value(GetKcal.getRecodedAt()));
+}
 
 }
