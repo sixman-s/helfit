6b1d723a3267957a5294976a1bdda97c8445442d
HooniKim <tpgns2289@naver.com>
2023-03-23 18:10:20 +0900

    🔨 로그인 에러처리 완료

--- front/src/components/loginc/LoginBox.tsx
+++ front/src/components/loginc/LoginBox.tsx
@@ -50,9 +50,14 @@ const LoginBox = () => {
             console.log(error);
           });
       })
-
-      .catch((res) => {
-        alert(res.message);
+      .catch((error) => {
+        if (error.response.status === 403) {
+          alert('회원가입시 입력한 이메일에서 인증을 완료해주세요.');
+        } else if (error.response.status === 400) {
+          alert('아이디와 비밀번호가 일치하지 않습니다.');
+        } else {
+          console.log(error);
+        }
       });
   };
 
