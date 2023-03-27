495197a66a261499ff90070ed410bd3f87674ded
jw_0819 <jw_0819@naver.com>
2023-03-28 00:27:55 +0900

    :art: 탈퇴 회원 정보 테이블 정보 저장시 암호화 기능 추가(AES256)

---
+++ back/helfit/src/main/java/sixman/helfit/domain/user/converter/CryptoConverter.java
@@ -0,0 +1,55 @@
+package sixman.helfit.domain.user.converter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import sixman.helfit.exception.BusinessLogicException;
+import sixman.helfit.exception.ExceptionCode;
+import sixman.helfit.utils.CryptoUtil;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter
+@Slf4j
+public class CryptoConverter implements AttributeConverter<String, String> {
+    @Value("${crypto.encrypt-key}")
+    private String ENCRYPTION_KEY;
+
+    private static final String ENCRYPT = "ENCRYPT";
+    private static final String DECRYPT = "DECRYPT";
+    private CryptoUtil cryptoUtil;
+
+    @Override
+    public String convertToDatabaseColumn(String sensitive) {
+        if (sensitive == null) return null;
+
+        return encryptDecryptString(sensitive, ENCRYPT);
+    }
+
+    @Override
+    public String convertToEntityAttribute(String sensitive) {
+        System.out.println("ENCRYPTION_KEY = " + ENCRYPTION_KEY);
+        if (sensitive == null) return null;
+
+        return encryptDecryptString(sensitive, DECRYPT);
+    }
+
+    private String encryptDecryptString(String sensitive, String encryptDecrypt) {
+        try {
+            if (cryptoUtil == null) cryptoUtil = new CryptoUtil(ENCRYPTION_KEY);
+
+            if (StringUtils.equalsIgnoreCase(encryptDecrypt, ENCRYPT))
+                return cryptoUtil.encrypt(sensitive);
+
+            if (StringUtils.equalsIgnoreCase(encryptDecrypt, DECRYPT))
+                return cryptoUtil.decrypt(sensitive);
+        } catch (Exception e) {
+            log.error("Exception: {}", e.getMessage());
+
+            throw new BusinessLogicException(ExceptionCode.INTERNAL_SERVER_ERROR);
+        }
+
+        return null;
+    }
+}
--- back/helfit/src/main/java/sixman/helfit/domain/withdraw/entity/Withdraw.java
+++ back/helfit/src/main/java/sixman/helfit/domain/withdraw/entity/Withdraw.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 import sixman.helfit.audit.Auditable;
+import sixman.helfit.domain.user.converter.CryptoConverter;
 import sixman.helfit.domain.user.entity.User;
 import sixman.helfit.security.entity.ProviderType;
 
@@ -20,12 +21,12 @@ public class Withdraw extends Auditable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long withdrawId;
 
-    @Column(length = 64)
     private String id;
 
+    @Convert(converter = CryptoConverter.class)
     private String email;
 
-    @Column(length = 30)
+    @Convert(converter = CryptoConverter.class)
     private String nickname;
 
     @Enumerated(EnumType.STRING)
