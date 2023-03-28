d12eee69fc861008bd2c1dfad19a909aeab27c1a
HooniKim <tpgns2289@naver.com>
2023-03-29 01:38:32 +0900

    UserProfile Image 수정

--- front/src/components/Community/C_Community/UserNav.tsx
+++ front/src/components/Community/C_Community/UserNav.tsx
@@ -12,9 +12,12 @@ const UserNav = () => {
     if (accessToken) {
       const userInfo = JSON.parse(localStorage.getItem('UserInfo'));
       if (userInfo) {
-        // add null check here
         setUserName(userInfo.nickname);
+      }
+      if (userInfo.profileImageUrl !== null) {
         setUserProfile(userInfo.profileImageUrl);
+      } else if (userInfo.profileImageUrl == null) {
+        setUserProfile(userProfile);
       }
     }
   }, []);
@@ -23,7 +26,10 @@ const UserNav = () => {
     <>
       <div className={style.UserProfile}>
         <div>
-          <img src={userProfile} className={style.UserPhoto} />
+          <img
+            src={userProfile || '../../assets/Community/UserProfile.svg'}
+            className={style.UserPhoto}
+          />
         </div>
         <div className={style.UserName1}>{userName}</div>
       </div>
