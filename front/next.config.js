1e3230530ad65c41cee0c8b345808e908a737f80
HooniKim <tpgns2289@naver.com>
2023-03-29 23:27:22 +0900

    oauth  오류 수정

--- front/next.config.js
+++ front/next.config.js
@@ -21,24 +21,24 @@ const nextConfig = {
       '/calendar': { page: '/calendar' },
       '/login': { page: '/login' },
       '/signup': { page: '/signup' },
-      '/mypage': { page: '/mypage' }
-      // '/oauth2/:path*': { page: '/oauth2/:path*' }
+      '/mypage': { page: '/mypage' },
+      '/oauth2': { page: '/oauth2/receive' }
     };
-  },
-  async redirects() {
-    return [
-      // {
-      //   source: '/oauth2/:path*',
-      //   destination: '/oauth2/:path*',
-      //   permanent: true
-      // },
-      {
-        source: '/oauth2/receive/:path*',
-        destination: '/oauth2/receive/:path*',
-        permanent: true
-      }
-    ];
   }
+  // async redirects() {
+  //   return [
+  //     // {
+  //     //   source: '/oauth2/:path*',
+  //     //   destination: '/oauth2/:path*',
+  //     //   permanent: true
+  //     // },
+  //     {
+  //       source: '/oauth2/receive:path*',
+  //       destination: '/oauth2/receive:path*',
+  //       permanent: true
+  //     }
+  //   ];
+  // }
 };
 
 module.exports = nextConfig;
