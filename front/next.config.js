b209c8a4636afcb10abfdc61eedd45745924ba08
MODAC0 <modac0302@gmail.com>
2023-03-29 11:44:25 +0900

    🚑

--- front/next.config.js
+++ front/next.config.js
@@ -15,10 +15,7 @@ const nextConfig = {
   ) {
     return {
       '/': { page: '/' },
-      '/map': { page: '/map' },
-      '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
-      '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
-      '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } }
+      '/map': { page: '/map' }
     };
   }
   // async redirects() {
