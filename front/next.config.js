9fa25d95bbf66cc7c21ba116bc1bd550e376c388
MODAC0 <modac0302@gmail.com>
2023-03-29 11:42:16 +0900

    :test: ci/cd test 완료

--- front/next.config.js
+++ front/next.config.js
@@ -9,20 +9,32 @@ const nextConfig = {
     });
     return config;
   },
-  async redirects() {
-    return [
-      {
-        source: '/login',
-        destination: '/login',
-        permanent: false
-      },
-      {
-        source: '/map',
-        destination: '/community',
-        permanent: false
-      }
-    ];
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/map': { page: '/map' },
+      '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
+      '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
+      '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } }
+    };
   }
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/login',
+  //       destination: '/login',
+  //       permanent: false
+  //     },
+  //     {
+  //       source: '/map',
+  //       destination: '/community',
+  //       permanent: false
+  //     }
+  //   ];
+  // }
 };
 
 module.exports = nextConfig;
