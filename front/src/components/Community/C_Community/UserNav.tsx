9c75c45a90053756e7343109955630a89e5f9a2d
HooniKim <tpgns2289@naver.com>
2023-03-30 17:29:06 +0900

    usernav 프로필 이미지 수정

--- front/src/components/Community/C_Community/UserNav.tsx
+++ front/src/components/Community/C_Community/UserNav.tsx
@@ -4,7 +4,7 @@ import style from '../../../styles/Community/C_Community.module.css';
 const UserNav = () => {
   const [userName, setUserName] = useState('User');
   const [userProfile, setUserProfile] = useState(
-    '../../../assets/Community/UserProfile.svg'
+    '../../../../assets/Community/UserProfile.svg'
   );
 
   useEffect(() => {
