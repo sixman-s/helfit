71647b0779472372e360426975f093bbd2ab5462
HooniKim <tpgns2289@naver.com>
2023-03-30 16:57:33 +0900

    next.config.js 수정

--- front/next.config.js
+++ front/next.config.js
@@ -22,24 +22,10 @@ const nextConfig = {
       '/login': { page: '/login' },
       '/signup': { page: '/signup' },
       '/mypage': { page: '/mypage' },
-      '/oauth2': { page: '/oauth2' },
+      // '/oauth2': { page: '/oauth2' },
       '/oauth2/receive': { page: '/oauth2/receive' }
     };
   }
-  // async redirects() {
-  //   return [
-  //     // {
-  //     //   source: '/oauth2/:path*',
-  //     //   destination: '/oauth2/:path*',
-  //     //   permanent: true
-  //     // },
-  //     {
-  //       source: '/oauth2/receive:path*',
-  //       destination: '/oauth2/receive:path*',
-  //       permanent: true
-  //     }
-  //   ];
-  // }
 };
 
 module.exports = nextConfig;
