f0dcd9080da1d5f576cf74ea78e8288694354ab2
jw_0819 <jw_0819@naver.com>
2023-05-11 12:08:00 +0900

    :art: 무중단 배포 테스트 완료 및 echo 구문 수정

--- back/helfit/scripts/server_validate.sh
+++ back/helfit/scripts/server_validate.sh
@@ -11,7 +11,7 @@ IDLE_PORT=$(echo "${PROFILE_AND_PORT}" | awk '{print $4}')
 
 for cnt in {1..10}
 do
-    echo "서버 응답 대기중 ... (${cnt}/10)";
+    echo "Docker : $ACTIVE_PROFILE 서비스 응답 대기중 ... (${cnt}/10)";
 
     UP=$(curl -s http://localhost:${ACTIVE_PORT}/api/v1/health | grep "blue\|green")
 
@@ -30,13 +30,13 @@ then
     exit 1
 fi
 
-echo "> Docker Nginx: Change profile and port - Reload"
+echo "Docker : Nginx reload"
 echo "set \$service_url http://spring-${ACTIVE_PROFILE}:${ACTIVE_PORT};" \
 | tee ${ROOT_PATH}/data/nginx/conf.d/service-url.inc \
 && docker cp ${ROOT_PATH}/data/nginx/conf.d/service-url.inc nginx:/etc/nginx/conf.d/service-url.inc \
 && docker exec nginx nginx -s reload
 
-echo "Docker : Idle $IDLE_PROFILE:$IDLE_PORT down"
-docker-compose -p spring-${IDLE_PROFILE} ${ROOT_PATH}/docker-compose.${IDLE_PROFILE}.yml down
+echo "Docker : Idle($IDLE_PROFILE:$IDLE_PORT) down"
+docker-compose -p spring-${IDLE_PROFILE} -f ${ROOT_PATH}/docker-compose.${IDLE_PROFILE}.yml down
 
-echo "$ACTIVE_PROFILE:$ACTIVE_PORT 배포가 정상적으로 완료되었습니다."
+echo "Docker : Active($ACTIVE_PROFILE:$ACTIVE_PORT) 배포가 정상적으로 완료되었습니다."
