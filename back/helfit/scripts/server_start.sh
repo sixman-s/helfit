5b051fb98d8b0c55c798c76dd38d26563b02704e
jw_0819 <jw_0819@naver.com>
2023-05-11 11:29:17 +0900

    :art: 무중단 배포 테스트 진행

--- back/helfit/scripts/server_start.sh
+++ back/helfit/scripts/server_start.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 source "$(dirname "$(readlink -f "$0")")/profile.sh"
 
-PROFILE_AND_PORT=$(find_profile_and_port)
-ACTIVE_PROFILE=$(echo "${PROFILE_AND_PORT}" | awk '{print $1}')
+ROOT_PATH="/home/ubuntu/build"
 
-docker-compose -p spring-${ACTIVE_PROFILE} -f /home/ubuntu/build/docker-compose.${ACTIVE_PROFILE}.yml up --build -d
+PROFILE_AND_PORT=$(find_profile_and_port)
+ACTIVE_PROFILE=$(echo "${PROFILE_AND_PORT}" | awk "{print $1}")
+
+docker-compose -p spring-${ACTIVE_PROFILE} -f ${ROOT_PATH}/docker-compose.${ACTIVE_PROFILE}.yml up --build -d
--- back/helfit/scripts/server_validate.sh
+++ back/helfit/scripts/server_validate.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 source "$(dirname "$(readlink -f "$0")")/profile.sh"
 
+ROOT_PATH="/home/ubuntu/build"
+
 PROFILE_AND_PORT=$(find_profile_and_port)
-ACTIVE_PROFILE=$(echo "${PROFILE_AND_PORT}" | awk '{print $1}')
-ACTIVE_PORT=$(echo "${PROFILE_AND_PORT}" | awk '{print $2}')
-IDLE_PROFILE=$(echo "${PROFILE_AND_PORT}" | awk '{print $3}')
-IDLE_PORT=$(echo "${PROFILE_AND_PORT}" | awk '{print $4}')
+ACTIVE_PROFILE=$(echo "${PROFILE_AND_PORT}" | awk "{print $1}")
+ACTIVE_PORT=$(echo "${PROFILE_AND_PORT}" | awk "{print $2}")
+IDLE_PROFILE=$(echo "${PROFILE_AND_PORT}" | awk "{print $3}")
+IDLE_PORT=$(echo "${PROFILE_AND_PORT}" | awk "{print $4}")
 
 for cnt in {1..10}
 do
     echo "서버 응답 대기중 ... (${cnt}/10)";
 
-    UP=$(curl -s https://localhost:${ACTIVE_PORT}/api/v1/health | grep 'blue\|green')
+    UP=$(curl -s https://localhost:${ACTIVE_PORT}/api/v1/health | grep "blue\|green")
 
     if [ -z "${UP}" ]
         then
@@ -30,11 +32,11 @@ fi
 
 echo "> Docker Nginx: Change profile and port - Reload"
 echo "set \$service_url http://spring-${ACTIVE_PROFILE}:${ACTIVE_PORT};" \
-| tee /home/ubuntu/build/data/nginx/conf.d/service-url.inc \
-&& docker cp /home/ubuntu/build/data/nginx/conf.d/service-url.inc nginx:/etc/nginx/conf.d/service-url.inc \
+| tee ${ROOT_PATH}/data/nginx/conf.d/service-url.inc \
+&& docker cp ${ROOT_PATH}/data/nginx/conf.d/service-url.inc nginx:/etc/nginx/conf.d/service-url.inc \
 && docker exec nginx nginx -s reload
 
 echo "Docker : Idle $IDLE_PROFILE:$IDLE_PORT down"
-docker-compose -p spring-${IDLE_PROFILE} ./docker-compose.${IDLE_PROFILE}.yml down
+docker-compose -p spring-${IDLE_PROFILE} ${ROOT_PATH}/docker-compose.${IDLE_PROFILE}.yml down
 
 echo "$ACTIVE_PROFILE:$ACTIVE_PORT 배포가 정상적으로 완료되었습니다."
