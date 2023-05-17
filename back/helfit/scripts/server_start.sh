3ea4df49ecc1214f5ec93cd4da8d483cd62d7439
jw_0819 <jw_0819@naver.com>
2023-05-17 20:19:29 +0900

    :art: nginx / prometheus 설정 수정

--- back/helfit/scripts/server_start.sh
+++ back/helfit/scripts/server_start.sh
@@ -6,4 +6,4 @@ ROOT_PATH="/home/ubuntu/build"
 PROFILE_AND_PORT=$(find_profile_and_port)
 ACTIVE_PROFILE=$(echo "${PROFILE_AND_PORT}" | awk '{print $1}')
 
-docker-compose -p spring-${ACTIVE_PROFILE} -f ${ROOT_PATH}/docker-compose.${ACTIVE_PROFILE}.yml up --build -d
+docker-compose -p spring-${ACTIVE_PROFILE} -f ${ROOT_PATH}/docker-compose.${ACTIVE_PROFILE}.yml up --build --force-recreate -d
